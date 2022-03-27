876612b899c98934ca52a2e164543895a7bb20b9
geoffrey521 <calvino.geoffrey@gmail.com>
2022-03-27 14:21:51 +0200

    Feature #9 : create fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\TaskFactory;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        TaskFactory::createMany(10);
+
+        UserFactory::createMany(10);
+
+        UserFactory::createOne([
+            'roles' => ['ROLE_ADMIN'],
+            'username' => 'admin',
+            'email' => 'admin@todo-co.fr',
+            'password' => 'admin'
+        ]);
+
+        TaskFactory::createMany(20, function () {
+            return [
+                'author' => UserFactory::random()
+            ];
+        });
+
+        $manager->flush();
+    }
+}
