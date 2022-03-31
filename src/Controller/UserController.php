37ba9e9faa8a2554b916cd88821829c5072b97a8
geoffrey521 <calvino.geoffrey@gmail.com>
2022-03-31 04:25:09 +0200

    Feature #5 : delete password hasher from crete controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -36,12 +36,6 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword(
-                $this->passwordHasher->hashPassword(
-                    $user,
-                    $user->getPassword()
-                )
-            );
             $em->persist($user);
             $em->flush();
 
