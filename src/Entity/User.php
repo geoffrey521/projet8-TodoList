3ee47dac24639631d3e50dd680d6f6f3075a1f8d
geoffrey521 <calvino.geoffrey@gmail.com>
2022-03-31 02:20:50 +0200

    Feature #5 : fix deprecated getUsername by getUserIdentifier

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,6 +80,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
      * @see PasswordAuthenticatedUserInterface
      */
     public function getPassword(): ?string
